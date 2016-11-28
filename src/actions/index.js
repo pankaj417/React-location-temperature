4d40a442176ab2eb810ff7557e3238dcb1bc88f0
pankaj <pankaj@onerent.co>
2016-11-28 09:48:57 -0800

    React-location-temperature

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const API_KEY = 'e94a7452cb9def1105229d0ec90d027d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
